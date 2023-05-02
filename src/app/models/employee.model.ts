6eed46c73718137af5f1d573a71774c282e9bab2
Madhu <madhuvarre444@gmail.com>
2023-05-02 08:20:35 -0400

    added by madhu

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,12 @@
+export class Employee {
+    id?: number = 0;
+    firstname:string = '';
+    lastname:string = '';
+    dob:string = '';
+    gender:string = '';
+    education:string = '';
+    company:string = '';
+    jobExperience:number = 0;
+    profile:string = '';
+  Salary: any;
+}  
